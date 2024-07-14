7a3b1278f44e143d1d3ca749d8e3987045013b55
Marco Martinez <martinezma025@gmail.com>
2024-07-14 00:44:31 -0500

    Woo! first commit

---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Marco Martinez on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location {
+                if let weather = weather {
+                    Text("Weather data fetched!")
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
+        .background(Color(hue: 0.638, saturation: 0.767, brightness: 0.296))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Marco Martinez on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
